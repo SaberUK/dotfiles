a58d5e7d9b1b1c5d64b8e2c89232086a74e3ad83
Peter Powell <petpow@saberuk.com>
2012-08-30 07:03:24 +0100

    Add ZSH options.

---
+++ _zsh/options.zsh
@@ -0,0 +1,8 @@
+# ==============================================================================
+# SaberUK's Dot Files <https://github.com/saberuk/dotfiles/>
+# ==============================================================================
+# Copyright (C) 2012 Peter "SaberUK" Powell <petpow@saberuk.com>
+# ==============================================================================
+
+setopt HIST_IGNORE_DUPS
+setopt INTERACTIVE_COMMENTS
