cbf6b2cca54a3c4c428b1cdaf818ec64b437622b
Peter Powell <petpow@saberuk.com>
2013-01-26 20:55:01 +0000

    Add alias for running Premake and then GNU Make.

--- _zsh/aliases.zsh
+++ _zsh/aliases.zsh
@@ -16,6 +16,11 @@ alias ll='ls -Al' # Show almost all entries, use long format.
 alias lr='ls -lR' # Use long format, list recursively.
 alias l='ls' # Shortcut.
 
+# Make aliases
+alias bmake='bsdmake'
+alias gmake='make'
+alias pregmake='premake4 gmake && gmake'
+
 # Windows compatibility aliases.
 alias cls='clear'
 alias dir='ls'
@@ -23,8 +28,6 @@ alias start='open'
 alias tracert='traceroute'
 
 # Misc aliases
-alias bmake='bsdmake'
-alias gmake='make'
 alias mkdir='mkdir -p' # Create intermediate directories as required.
 alias preview='open -a Preview'
 alias ssp='ssh -D 12468' # Create a local SOCKS server which tunnels over SSH.
