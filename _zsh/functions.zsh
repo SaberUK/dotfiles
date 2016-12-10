7dc36ae74ed5ffdea7cbd54d33d1c8ea199b7a2a
Peter Powell <petpow@saberuk.com>
2016-12-10 03:58:55 +0000

    Make safari-cleanup clear some more stuff.

--- _zsh/functions.zsh
+++ _zsh/functions.zsh
@@ -13,6 +13,8 @@ function safari-cleanup
 {
 	akill Safari force
 	rm -f ~/Library/Safari/History*
+	rm -f ~/Library/Safari/LastSession.plist
+	rm -f ~/Library/Safari/RecentlyClosedTabs.plist
 	rm -f ~/Library/Caches/com.apple.Safari/Cache.db*
 	rm -rf ~/Library/Caches/com.apple.Safari/Webpage\ Previews
 	rm -rf ~/Library/Caches/Metadata/Safari/History
